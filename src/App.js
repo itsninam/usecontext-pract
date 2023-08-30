dbab18fd8aa5536c1bbc6d4802b6704100162fdc
Madalina <madalina@Madalinas-MacBook-Air.local>
2023-08-30 19:03:06 -0400

    Create header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <Results />
+        <SearchBlogs />
+      </Header>
     </div>
   );
 }
 
 export default App;
+
+function Header({ children }) {
+  return (
+    <header>
+      <Logo />
+      <div className="right-container">{children}</div>
+    </header>
+  );
+}
+
+function Logo() {
+  return <h1>⚛️ Atomic Blog</h1>;
+}
+
+function Results() {
+  return <p>🚀 X blogs found</p>;
+}
+
+function SearchBlogs() {
+  return (
+    <>
+      <form>
+        <label htmlFor="search-blogs">Search blogs: </label>
+        <input type="text" id="search-blogs" />
+      </form>
+      <button>Clear posts</button>
+    </>
+  );
+}
